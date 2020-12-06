5be791ad77af43594162957db6ca334b67b9b3ee
Javier Bauset <baunes@protonmail.com>
2020-12-06 10:53:21 +0100

    build: add Gradle scripts

--- server/src/main/java/com/sparta/controller/MainController.java
+++ server/src/main/java/com/sparta/controller/MainController.java
@@ -6,15 +6,19 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+
 @RestController
 public class MainController {
 
   @PostMapping("/load/{provider}")
   public int load(@PathVariable("provider") String provider, @RequestBody byte[] content) throws IOException {
+    return -1;
   }
 
   @GetMapping("/data/{provider}/total")
   public int total(@PathVariable("provider") String provider) {
+    return -1;
   }
 
 }
